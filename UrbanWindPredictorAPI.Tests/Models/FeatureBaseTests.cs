2bc2cfbfe4c09aa0796b4b89b8b591681933b721
ZackAkil <zackakil94@gmail.com>
2016-03-19 17:34:18 +0000

    Added featurebase class for prediction calculation

---
+++ UrbanWindPredictorAPI.Tests/Models/FeatureBaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UrbanWindPredictorAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrbanWindPredictorAPI.Models.Tests
+{
+    [TestClass()]
+    public class FeatureBaseTests
+    {
+        [TestMethod()]
+        public void FeatureBaseTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ UrbanWindPredictorAPI/Models/FeatureBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UrbanWindPredictorAPI.Models
+{
+    public class FeatureBase
+    {
+        public double multiplyValue { get; set; }
+        public double powerValue { get; set; }
+
+        public FeatureBase(Bias DbBias)
+        {
+            this.powerValue = 1;
+            this.multiplyValue = DbBias.value;
+        }
+
+        public FeatureBase(Feature DbFeature)
+        {
+            this.multiplyValue = DbFeature.multiValue;
+            this.powerValue = DbFeature.powValue;
+        }
+
+        public static double getPrediction(FeatureBase[] features, double )
+        {
+
+            double result = 0;
+
+            foreach (var feature in features)
+            {
+                //accumulate features --  needs sigmoid-- use matrix math library when it inported.
+                result += Math.Pow((feature.multiplyValue * 1) , feature.powerValue);
+            }
+
+
+            return 0.0;
+        }
+
+
+    }
+
+}
